1cacc2ac7808c031906b60863590cb3d98b3911a
Leo Ganas <leoganas.jr@gmail.com>
2025-02-23 21:27:04 +0800

    Added Login and Sign up Form with favicon

---
+++ RoadSense/Components/Pages/Common/Models/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoadSense.Components.Pages.Common.Models;
+public class UserLogin
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+
+    [Required(ErrorMessage = "Username is required.")]
+    [MinLength(1, ErrorMessage = "Username cannot be empty.")]
+    public string UserName { get; set; }
+
+    [Required]
+    [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+    public string Password { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
