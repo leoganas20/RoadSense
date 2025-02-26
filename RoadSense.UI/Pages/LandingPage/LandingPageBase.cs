1f63797142127e7e602937694689bfb17e4cddcd
PC <PC@DESKTOP-IMS3VDB>
2025-02-26 14:53:15 +0800

    Changes made in landing page static data

--- RoadSense.UI/Pages/LandingPage/LandingPageBase.cs
+++ RoadSense.UI/Pages/LandingPage/LandingPageBase.cs
@@ -5,45 +5,27 @@ namespace RoadSense.UI.Pages.LandingPage;
 
 public class LandingPageBase : ComponentBase
 {
-   
+
 
     protected List<Service> Services = new()
         {
             new Service
             {
-                Title = "Pediatricians",
-                Details = "Medical specialists focused on the care and treatment of infants, children, and adolescents.",
-                
+                Title = "Real-Time Monitoring",
+                Details = "Dashboard displaying live data on overspeeding and honking violations.",
+
             },
             new Service
             {
-                Title = "Plastic Surgeons",
-                Details = "Specialists in reconstructive and aesthetic surgery to improve or restore appearance or function.",
-                
+                Title = "Violation Tracking",
+                Details = "Records of detected violations, including speed, honking, date, time, and location.",
+
             },
             new Service
             {
-                Title = "Ophthalmologists",
-                Details = "Medical doctors who specialize in the diagnosis, treatment, and surgery of eye-related conditions.",
-                
-            },
-            new Service
-            {
-                Title = "Orthopedic",
-                Details = "Medical specialists in the diagnosis and treatment of musculoskeletal system disorders, including bones, joints, and muscles.",
-                
-            },
-            new Service
-            {
-                Title = "Gastroenterologist",
-                Details = "Specialists focused on diagnosing and treating conditions of the digestive system, including the stomach, intestines, and liver.",
-                
-            },
-            new Service
-            {
-                Title = "Cardiology",
-                Details = "Medical field specializing in diagnosing and treating heart conditions, including coronary artery disease, heart failure, and arrhythmias.",
-               
+                Title = "Reports & Analytics",
+                Details = "Daily, weekly, and monthly violation reports. Graphs and statistics for trend analysis.",
+
             },
         };
 
